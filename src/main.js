51a23d3c4a6a3dcf1f4e0f660cb470af71acc424
IyadAbed <iyadrabed@gmail.com>
2023-05-01 23:29:52 +0300

    hello

---
+++ src/main.js
@@ -0,0 +1,19 @@
+
+const initState = {
+  accounts : [
+          {
+            id: 1,
+            customerName:"Israa Othman",
+            accountNumber: "123456",
+            accountType: "Savings"
+          },
+          {
+            id: 2,
+            customerName:"Ahmad Zahran",
+            accountNumber: "987654",
+            accountType: "Student accounts"
+          }
+  ]
+
+
+}
